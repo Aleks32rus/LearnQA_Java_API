addef2d5c6897a2a12b02e0c4e6bcffcd9d6dd9f
Alexey Erokhov <ean1@bk.ru>
2022-09-08 23:29:41 +0300

    add first test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,3 +1,5 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
@@ -6,4 +8,13 @@ public class HelloWorldTest {
     public void testHelloWorld() {
         System.out.println("Hello from Aleksey");
     }
+
+    @Test
+    public void testFirst() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+
 }
