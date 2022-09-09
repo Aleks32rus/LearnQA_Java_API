631f590ac8f28f6a2f1472877b52e0204e2548dd
Alexey Erokhov <ean1@bk.ru>
2022-09-09 09:09:26 +0300

    add first test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -14,7 +14,7 @@ public class HelloWorldTest {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
-        response.prettyPrint();
+        System.out.println(response.asString());
     }
 
 }
