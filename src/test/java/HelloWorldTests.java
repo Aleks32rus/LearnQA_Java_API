a67b5be4a6bb12965b4047a57e819270df65da0b
Alexey Erokhov <ean1@bk.ru>
2022-10-08 22:28:49 +0300

    add Ex14

--- src/test/java/HelloWorldTests.java
+++ src/test/java/HelloWorldTests.java
@@ -1,5 +1,4 @@
 import io.restassured.RestAssured;
-import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
@@ -14,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public class HelloWorldTest {
+public class HelloWorldTests {
 
     @Test
     public void testHelloWorld() {
